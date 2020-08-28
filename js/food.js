7d97612239a64606c6d424470f63fedb50488d8a
ShauryaKaushik <65485931+ShauryaKaushik@users.noreply.github.com>
2020-08-28 20:17:59 +0530

    Add files via upload

---
+++ js/food.js
@@ -0,0 +1,63 @@
+class Food{
+
+constructor(){
+
+this.foodStock = 0;
+this.lastFed = null;
+this.image = loadImage("Milk.png");
+}
+
+getFoodStock(){
+
+    return this.foodStock;
+}
+
+updateFoodStock(foodStock){
+    
+this.foodStock = foodStock
+    
+}
+
+deductFood(){
+
+if(this.foodStock > 0.3){
+   this.foodStock = this.foodStock - 1
+ }    
+        
+}
+
+display(){
+
+var x = 80,y = 100; 
+
+imageMode(CENTER);
+image(this.image,720,220,70,70);
+
+  if(this.foodStock!=0){
+
+    for(var i = 0;i<this.foodStock;i++){
+      if(i%10===0){
+       x = 80;
+       y = y+50;
+      }
+      image(this.image,x,y,50,50);
+      x = x+30;
+    }
+
+ } 
+
+}
+
+bedroom(){
+ background(bedroomImg,500,500);
+}
+
+washroom(){
+ background(washroomImg,550,500);
+}
+
+garden(){
+ background(gardenImg,550,500);
+}
+
+}
